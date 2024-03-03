a8d9675cf33908e5270b06c209927cb1746b0725
ritiksharma142001 <sharma.ritik2023@gmail.com>
2024-03-03 13:33:36 +0530

    create listing page ui

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
       <ToastContainer
